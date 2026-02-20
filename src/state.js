8a000c2962a974dc970c6fcb37fab9f63e89e585
axlgoze <rbaxel.f@gmail.com>
2026-02-19 19:47:18 -0600

    1)add(src/state.js):add SOT logic[JIRA-1234]

--- src/state.js
+++ src/state.js
@@ -2,10 +2,12 @@ import { createLog } from './logLogic';
 import { createTask } from './taskLogic';
 
 
-const state = {
+const VALID_FILTERS = ['all', 'pending', 'completed'];
+
+export const _state = {
     tasks : [],
     logs : [],
-    filter: 'all'
+    filter: VALID_FILTERS[0],
     
     // deleteTask : (taskId) => {},
     // editTask : (taskId) => {},
@@ -14,12 +16,33 @@ const state = {
     // generateLog : (taskId) => {},
 }
 
+export function getSate(){
+    const currentState = structuredClone(_state);
+    return currentState;
+}
+
+export function getTasks(){
+    const currentTasks = structuredClone(_state.tasks);
+    return currentTasks
+}
+
+function getLogs(){
+    const currentLogs = structuredClone(_state.logs);
+    return currentLogs;
+}
+
+export function setFilter(newFilter){
+    if (VALID_FILTERS.includes(newFilter)) {
+        _state.filter = filter;
+    }
+}
+
 
 export const addTaskToState = (title) => {
         const newTask = createTask(title);
         const newLog = createLog(newTask.id, 'CREATE', newTask.title);
-        state.tasks.push(newTask);
-        state.logs.push(newLog);
+        _state.tasks.push(newTask);
+        _state.logs.push(newLog);
         console.log("✅ Task & Log added to state");
         return newTask;
     };
